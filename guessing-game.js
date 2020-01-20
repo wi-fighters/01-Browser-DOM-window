5fa2b10930104091f3ab22609064417d9932ce6f
Eva Larumbe <evalarumbe@gmail.com>
2020-01-20 13:44:02 +0100

    Wrap the pseudocode in an IIFE

--- guessing-game.js
+++ guessing-game.js
@@ -1,23 +1,25 @@
-const promptForGuesses = number => {
-    // initialize attempts to 0 // 3
-    // initialize the prompt message "Guess a number between 1 and 10"
-
-    // while (attempts < 3) {
-        // prompt the user with the prompt message
-        // increment the number of attempts
-
-        // if (the user guesses a correct number)
-            // print success message and number of attempts
-            // stop the loop (TODO: break? return?)
-
-        // else 
-            // update prompt message: "Nope, sorry, try again"
-    // }
-
-    // assuming we didn't return before
-    // print fail message: "Sorry, you failed to guess the number in three attempts. The number was 2!"
-};
-
-const randomNumber = Math.floor(Math.random() * 10) + 1;
-
-promptForGuesses(randomNumber);
+(() => {
+    const promptForGuesses = number => {
+        // initialize attempts to 0 // 3
+        // initialize the prompt message "Guess a number between 1 and 10"
+    
+        // while (attempts < 3) {
+            // prompt the user with the prompt message
+            // increment the number of attempts
+    
+            // if (the user guesses a correct number)
+                // print success message and number of attempts
+                // stop the loop (TODO: break? return?)
+    
+            // else 
+                // update prompt message: "Nope, sorry, try again"
+        // }
+    
+        // assuming we didn't return before
+        // print fail message: "Sorry, you failed to guess the number in three attempts. The number was 2!"
+    };
+    
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    
+    promptForGuesses(randomNumber);    
+})();
