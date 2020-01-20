7320a7351b40ae81ac5f166a17af60c34a693806
Eva Larumbe <evalarumbe@gmail.com>
2020-01-20 16:53:57 +0100

    Add solution according to pseudocode

--- guessing-game.js
+++ guessing-game.js
@@ -1,25 +1,26 @@
 (() => {
     const promptForGuesses = number => {
-        // initialize attempts to 0 // 3
-        // initialize the prompt message "Guess a number between 1 and 10"
+        let attempts = 0;
+        let message = "Guess a number between 1 and 10";
+        let currentGuess = '';
     
-        // while (attempts < 3) {
-            // prompt the user with the prompt message
-            // increment the number of attempts
+        while (attempts < 3) {
+            currentGuess = parseInt(prompt(message), 10);
+            console.log('currentGuess', currentGuess);
+            attempts += 1;
     
-            // if (the user guesses a correct number)
-                // print success message and number of attempts
-                // stop the loop (TODO: break? return?)
+            if (currentGuess === number) {
+                return `Success! The number was ${number}! Attempts: ${attempts}`;
+            } else {
+                message = 'Nope, sorry! Try again!'
+            }
+        }
     
-            // else 
-                // update prompt message: "Nope, sorry, try again"
-        // }
-    
-        // assuming we didn't return before
-        // print fail message: "Sorry, you failed to guess the number in three attempts. The number was 2!"
+        return `Sorry, you failed to guess the number in three attempts. The number was ${number}!`;
     };
     
     const randomNumber = Math.floor(Math.random() * 10) + 1;
     
-    promptForGuesses(randomNumber);    
+    const outcome = promptForGuesses(randomNumber);
+    console.log(outcome);    
 })();
